443bf1d021f508c0decfe72c0bb5b24d9a8274d6
icons <65128346+gogogojason@users.noreply.github.com>
2021-02-03 01:45:11 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -7,9 +7,9 @@ svn co https://github.com/gogogojason/files/trunk/Sim_RM2100 ./devices/Sim_RM210
 svn co https://github.com/gogogojason/files/trunk/x86_64 ./devices/x86_64
 svn co https://github.com/garypang13/OpenWrt/trunk/devices/common ./devices/common
 sed -i 's/GaryPang/hfy166/g' ./devices/common/diy/package/base-files/files/etc/banner
-wget https://raw.githubusercontent.com/gogogojason/files/master/common/files/etc/config/AdGuardHome.yaml ./devices/common/files/etc/config/AdGuardHome.yaml
+wget -P ./devices/common/files/etc/config https://raw.githubusercontent.com/gogogojason/files/master/common/files/etc/config/AdGuardHome.yaml
 rm -f ./devices/common/files/etc/config/mwan3
-wget https://raw.githubusercontent.com/gogogojason/files/master/common/files/etc/config/mwan3 ./devices/common/files/etc/config/mwan3
+wget -P ./devices/common/files/etc/config https://raw.githubusercontent.com/gogogojason/files/master/common/files/etc/config/mwan3
 find ./devices/common/.config -name "*bypass*" | xargs -i sed -i "s/bypass//g" {}
 find ./devices/common/diy.sh -name "*GaryPang*" | xargs -i sed -i "s/bypass//g" {}
 sed -i "18a\CONFIG_PACKAGE_luci-app-poweroff=y" ./devices/common/.config
@@ -17,8 +17,8 @@ sed -i "80a\chmod 755 /sbin/reboot" ./devices/common/default-settings
 sed -i "80a\chmod 755 /sbin/ethinfo" ./devices/common/default-settings
 sed -i 's/10.0.0.1/192.168.2.1/g' ./devices/common/default-settings
 sed -i "s/$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/g" ./devices/common/default-settings
-wget https://raw.githubusercontent.com/gogogojason/files/master/diy2.sh ./devices/common/diy2.sh
+wget -P ./devices/common https://raw.githubusercontent.com/gogogojason/files/master/diy2.sh
 rm -f ./devices/common/settings.ini
-wget https://github.com/gogogojason/files/blob/master/settings.ini ./devices/common/settings.ini
+wget -P ./devices/common https://github.com/gogogojason/files/blob/master/settings.ini
 
 exit 0
