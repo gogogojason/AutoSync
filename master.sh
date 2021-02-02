bb222964dbe707650c3bc48bbdba49261a2fc409
icons <65128346+gogogojason@users.noreply.github.com>
2021-02-03 01:57:19 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -2,7 +2,7 @@
 mkdir devices
 svn co https://github.com/gogogojason/files/trunk/All_RM2100 ./devices/All_RM2100
 svn co https://github.com/gogogojason/files/trunk/Ap_RM2100 ./devices/Ap_RM2100
-svn co https://github.com/gogogojason/files/trunk/Mi2100 ./devices/devices
+svn co https://github.com/gogogojason/files/trunk/Mi2100 ./devices/Mi2100
 svn co https://github.com/gogogojason/files/trunk/Sim_RM2100 ./devices/Sim_RM2100
 svn co https://github.com/gogogojason/files/trunk/x86_64 ./devices/x86_64
 svn co https://github.com/garypang13/OpenWrt/trunk/devices/common ./devices/common
@@ -10,8 +10,8 @@ sed -i 's/GaryPang/hfy166/g' ./devices/common/diy/package/base-files/files/etc/b
 wget -P ./devices/common/files/etc/config https://raw.githubusercontent.com/gogogojason/files/master/common/files/etc/config/AdGuardHome.yaml
 rm -f ./devices/common/files/etc/config/mwan3
 wget -P ./devices/common/files/etc/config https://raw.githubusercontent.com/gogogojason/files/master/common/files/etc/config/mwan3
-find ./devices/common/.config -name "*bypass*" | xargs -i sed -i "s/"*bypass*"//g" {}
-find ./devices/common/diy.sh -name "*GaryPang*" | xargs -i sed -i "s/"*GaryPang*"//g" {}
+find devices/common/.config -name "*bypass*" | xargs -i sed -i "s/"*bypass*"//g" {}
+find devices/common/diy.sh -name "*GaryPang*" | xargs -i sed -i "s/"*GaryPang*"//g" {}
 sed -i "18a\CONFIG_PACKAGE_luci-app-poweroff=y" ./devices/common/.config
 sed -i "80a\chmod 755 /sbin/reboot" ./devices/common/default-settings
 sed -i "80a\chmod 755 /sbin/ethinfo" ./devices/common/default-settings
