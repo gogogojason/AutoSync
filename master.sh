b9974911ffdb01a05e4b98a372c968c164c15fdb
icons <65128346+gogogojason@users.noreply.github.com>
2021-02-03 02:03:07 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -10,13 +10,13 @@ sed -i 's/GaryPang/hfy166/g' ./devices/common/diy/package/base-files/files/etc/b
 wget -P ./devices/common/files/etc/config https://raw.githubusercontent.com/gogogojason/files/master/common/files/etc/config/AdGuardHome.yaml
 rm -f ./devices/common/files/etc/config/mwan3
 wget -P ./devices/common/files/etc/config https://raw.githubusercontent.com/gogogojason/files/master/common/files/etc/config/mwan3
-find devices/common/.config -name "*bypass*" | xargs -i sed -i "s/"*bypass*"//g" {}
-find devices/common/diy.sh -name "*GaryPang*" | xargs -i sed -i "s/"*GaryPang*"//g" {}
+sed -i '/bypass/d' ./devices/common/.config
+sed -i '/GaryPang/d' ./devices/common/.config
 sed -i "18a\CONFIG_PACKAGE_luci-app-poweroff=y" ./devices/common/.config
 sed -i "80a\chmod 755 /sbin/reboot" ./devices/common/default-settings
 sed -i "80a\chmod 755 /sbin/ethinfo" ./devices/common/default-settings
 sed -i 's/10.0.0.1/192.168.2.1/g' ./devices/common/default-settings
-sed -i "s/$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/g" ./devices/common/default-settings
+sed -i "s/1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336/1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/g" ./devices/common/default-settings
 wget -P ./devices/common https://raw.githubusercontent.com/gogogojason/files/master/diy2.sh
 rm -f ./devices/common/settings.ini
 wget -P ./devices/common https://github.com/gogogojason/files/blob/master/settings.ini
